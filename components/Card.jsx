e45910c6e3a2f43fdae56e2a24e498600a12971a
simondelasheras <simidlh@gmail.com>
2023-11-10 00:15:44 -0300

    se agregaron nuestros productos con las secciones de men y women

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import styles from "../styles/Card.module.css"
+import styles from "../styles/Card.module.css";
+import Link from 'next/link';
 
 function ImgOverlayExample() {
   return (
@@ -8,26 +9,30 @@ function ImgOverlayExample() {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <Card className={`bg-dark text-white ${styles.firstCard}`}>
-              <Card.Img src="https://i.imgur.com/Ng1iHXO.jpg" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title className={styles.cardTitle}>Men Section</Card.Title>
-                <Card.Text>
-                  {/* Contenido de la primera tarjeta */}
-                </Card.Text>
-              </Card.ImgOverlay>
-            </Card>
+            <Link href="#men-section">
+                <Card className={`bg-dark text-white ${styles.firstCard}`}>
+                  <Card.Img src="https://i.imgur.com/Ng1iHXO.jpg" alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title className={styles.cardTitle}>Men Section</Card.Title>
+                    <Card.Text>
+                      {/* Contenido de la primera tarjeta */}
+                    </Card.Text>
+                  </Card.ImgOverlay>
+                </Card>
+            </Link>
           </div>
           <div className="col-md-6">
-            <Card className={`bg-dark text-white ${styles.secondCard}`}>
-              <Card.Img src="https://i.imgur.com/LasBh0t.jpg" alt="Card image" />
-              <Card.ImgOverlay>
-                <Card.Title className={styles.cardTitle}>Women Section</Card.Title>
-                <Card.Text>
-                  {/* Contenido de la segunda tarjeta */}
-                </Card.Text>
-              </Card.ImgOverlay>
-            </Card>
+            <Link href="#women-section">
+                <Card className={`bg-dark text-white ${styles.secondCard}`}>
+                  <Card.Img src="https://i.imgur.com/LasBh0t.jpg" alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title className={styles.cardTitle}>Women Section</Card.Title>
+                    <Card.Text>
+                      {/* Contenido de la segunda tarjeta */}
+                    </Card.Text>
+                  </Card.ImgOverlay>
+                </Card>
+            </Link>
           </div>
         </div>
       </div>
@@ -36,3 +41,4 @@ function ImgOverlayExample() {
 }
 
 export default ImgOverlayExample;
+
